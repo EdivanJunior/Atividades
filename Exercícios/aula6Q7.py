090c774b28938bf593412c8da5c96e38d72d7c15
Edivan Júnior <112666772+EdivanJunior@users.noreply.github.com>
2022-09-27 21:16:00 -0300

    Add files via upload

---
+++ Exercícios/aula6Q7.py
@@ -0,0 +1,35 @@
+cadastros = []
+
+botao = 1000
+while botao != 0:
+    print ("Digite 1 para cadastrar um novo usuário")
+    print ("Digite 2 para imprimir todos os usuários")
+    print ("Digite 0 para sair")
+    botao = int (input("Digite a opção desejada:"))
+
+    if botao == 1:
+
+        # Entrada dos dados
+        nome = input("Digite o nome: ")
+        idade = int(input("digite a idade: "))
+        email = input("Digite o e-mail: ")
+
+        # Folha decadastro
+        dados = [nome,idade,email]
+
+        # Guardando a folha na pasta 
+        cadastros.append(dados)
+
+
+    elif botao == 2:
+        for p in cadastros:
+            print (p)
+    elif botao == 0:
+        print("Obrigado por acessar este software!")
+
+    else: 
+        print("Digite um número válido!")
+
+
+
+
